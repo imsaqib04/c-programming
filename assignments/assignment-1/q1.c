0edb9055e04174a142d8eb6ec55aff9a24d64d7f
imsaqib04 <157666382+imsaqib04@users.noreply.github.com>
2024-01-31 17:00:27 +0530

    q1

--- assignments/assignment-1/q1.c
+++ assignments/assignment-1/q1.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
-int main()
-{
-printf("Hello World");
-return 0;
+#include<stdio.h>
+
+int main(){
+
+	double num1 = 2147483600;
+	double num2 = 100;
+	double sum  = num1 + num2;
+
+	printf("%.2f\n",sum);
+	return 0;
 }
