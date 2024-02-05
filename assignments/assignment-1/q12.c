46ecab6891d98f47966eddb9c141dbb5170445d4
imsaqib04 <157666382+imsaqib04@users.noreply.github.com>
2024-02-05 19:51:56 +0530

    Update q12.c

--- assignments/assignment-1/q12.c
+++ assignments/assignment-1/q12.c
@@ -1,33 +1,47 @@
-#include<stdio.h>
-void main(){
- int option;
- int result;
- int a,b;
- printf("choose options\n 1. add\n 2. sub\n 3. sub\n 4. div\n 5. exit\n");
- scanf("%d\n",&option);
- printf("enter the value of a\n");
- scanf("%d\n",&a);
- printf("enter the value of b\n");
- scanf("%d\n",&b);
- while(1){
-
-    switch(option){
-    case 1:
-           result = a+b;
-    break;
-    case 2:
-           result = a-b;
-    break;
-    case 3: 
-           result = a*b;
-    break;
-    case 4: 
-           result = a/b;
-    break;
-
-    default:
-    printf("invalid option");
-  }
-  }
- printf("%d\n",result);
-  }
+/*C program to design calculator with basic operations using switch.*/
+ 
+#include <stdio.h>
+ 
+int main()
+{
+    int num1,num2;
+    float result;
+    char ch;    //to store operator choice
+     
+    printf("Enter first number: ");
+    scanf("%d",&num1);
+    printf("Enter second number: ");
+    scanf("%d",&num2);
+     
+    printf("Choose operation to perform (+,-,*,/,%): ");
+    scanf(" %c",&ch);
+     
+    result=0;
+    switch(ch)    
+    {
+        case '+':
+            result=num1+num2;
+            break;
+             
+        case '-':
+            result=num1-num2;
+            break;
+         
+        case '*':
+            result=num1*num2;
+            break;
+             
+        case '/':
+            result=(float)num1/(float)num2;
+            break;
+             
+        case '%':
+            result=num1%num2;
+            break;
+        default:
+            printf("Invalid operation.\n");
+    }
+ 
+    printf("Result: %d %c %d = %f\n",num1,ch,num2,result);
+    return 0;
+}
