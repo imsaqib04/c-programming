b7ae3d5dbe8edc0798db9f2d7c1a517ff11ff1be
imsaqib04 <157666382+imsaqib04@users.noreply.github.com>
2024-02-05 23:31:23 +0530

    q3

---
+++ assignments/assignment-4/q3.c
@@ -0,0 +1,25 @@
+//array
+
+#include<stdio.h>
+int main(){
+
+    int size = 10;
+    int a[10] = {10,20,30,40,50,60,70,80,90,100};
+    int x;
+    printf("enter any value in given arr elements\n");
+    scanf("%d",&x);
+    int index = linear_search(a,size,x);
+    printf("%d",index);
+    return 0;
+}
+
+
+int linear_search(int a[],int size, int x){
+    int i;
+        for(i = 0; i<size; i++){
+            if(a[i] ==x){
+                return i;
+            }
+        }
+        return -1;
+}
